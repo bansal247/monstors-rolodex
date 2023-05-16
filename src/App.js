aeb8f3a992e773eb5f9f67943288eecdbfc4256e
Shashwat Bansal <66252519+bansal247@users.noreply.github.com>
2023-05-16 12:37:11 +0530

    craeted monstor rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  constructor(){
+    super();
+
+  //   this.state = {
+  //     monsters: [
+  //     {
+  //       name:'Linda'
+  //     },
+  //     {
+  //       name:'Frank'
+  //     },
+  //     {
+  //       name:'Jacky'
+  //     },
+  //   ]
+  // };
+      this.state = {
+        monsters:[],
+        searchField:"",
+      };
+      // console.log('constructor');
+
+  }
+
+  componentDidMount(){ // only happens one time
+    // console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((respose)=>respose.json())
+    .then((users)=> this.setState(()=>{
+      return {monsters:users};
+    }
+    )
+    )
+  }
+
+  onSearchChange = (event)=>{
+    const searchField = event.target.value.toLowerCase()
+    this.setState(
+      ()=>{
+        return {searchField}
+      }
+    )
+
+  } // react will create anonymous function again and again so we are create a non anonymous function
+
+  render(){
+    // console.log('render');
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLowerCase().includes(searchField);
+    });
+    return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox 
+      className = 'monsters-search-box'
+      onChangeHandler = {onSearchChange}
+      placeholder = 'search monster' />
+      <CardList monsters = {filteredMonsters}/>
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
